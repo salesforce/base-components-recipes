58e6bcaca13ab5705e7f0205908bb5e387fca9a5
tok-sfci9 <tok_sfci9@salesforce.com>
2019-10-18 16:06:31 +0000

    recipes update

---
+++ force-app/main/default/lwc/accordionUtilsPrivate/singleOpenSectionStrategy.js
@@ -0,0 +1,65 @@
+function openSection(sectionList, sectionToOpen) {
+  let sectionChanged = false;
+
+  if (sectionToOpen) {
+    const sections = sectionList.sections;
+    sections.forEach(section => {
+      if (section !== sectionToOpen) {
+        sectionChanged = sectionChanged || section.isOpen();
+        section.close();
+      }
+    });
+    sectionChanged = sectionChanged || !sectionToOpen.isOpen();
+    sectionToOpen.open();
+  }
+
+  return sectionChanged;
+}
+
+export class SingleOpenSectionStrategy {
+  constructor(sectionList) {
+    this.privateSectionList = sectionList;
+  }
+
+  openFirstSection() {
+    const sections = this.privateSectionList.sections;
+
+    if (sections.length) {
+      return openSection(this.privateSectionList, sections[0]);
+    }
+
+    return false;
+  }
+
+  openSectionByName(name) {
+    const normalizedName =
+      Array.isArray(name) && name.length > 0 ? name[0] : name;
+    const sectionToOpen = this.privateSectionList.getSectionByName(
+      normalizedName
+    );
+
+    return openSection(this.privateSectionList, sectionToOpen);
+  }
+
+  handleSectionSelect(sectionId) {
+    const selectedSection = this.privateSectionList.getSectionById(sectionId);
+
+    return openSection(this.privateSectionList, selectedSection);
+  }
+
+  handleSectionWillDeregister(sectionId) {
+    const sectionToDelete = this.privateSectionList.getSectionById(sectionId);
+
+    if (sectionToDelete.isOpen()) {
+      const sectionToOpen = this.privateSectionList.getNextSectionTo(sectionId);
+
+      if (sectionToOpen && sectionToOpen !== sectionToDelete) {
+        sectionToOpen.open();
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+}
