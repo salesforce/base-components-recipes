3af73f77a922f10b87b25fea46e8263b7b5ca102
Stephen Gesin <sgesin@salesforce.com>
2019-10-29 08:51:48 -0700

    removing formattedRichText

--- force-app/main/default/lwc/purifyLib/purifyLib.js
+++
@@ -1,5 +0,0 @@
-import purify from './purify';
-
-export default function sanitizeHTML(dirty, config) {
-    return purify.sanitize(dirty, config);
-}
--- force-app/main/lwc/formattedRichTextRecipes/formattedRichTextRecipes.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class FormattedRichTextRecipes extends LightningElement {
-    richtext = '<h2>Default <s>Value</s></h2>';
-}
