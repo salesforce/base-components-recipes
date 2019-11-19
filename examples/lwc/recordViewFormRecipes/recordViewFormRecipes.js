7179293f7dceac8c3bed9fd4c59be266fe30516a
muenzpraeger <mail@winkelmeyer.com>
2019-11-19 09:53:25 -0800

    Prettier formatting

--- examples/lwc/recordViewFormRecipes/recordViewFormRecipes.js
+++ examples/lwc/recordViewFormRecipes/recordViewFormRecipes.js
@@ -5,17 +5,17 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 import { LightningElement, track, wire } from 'lwc';
-import getRecord  from '@salesforce/apex/GetAccountId.GetAccountId';
+import getRecord from '@salesforce/apex/GetAccountId.GetAccountId';
 
 export default class RecordViewFormRecipes extends LightningElement {
     @track accountId;
 
     @wire(getRecord)
     wiredProperty(value) {
-        if(value.data) {
+        if (value.data) {
             this.accountId = value.data.Id;
         } else if (value.error) {
-            console.log("OOOPS: ", value.error)
+            console.log('OOOPS: ', value.error);
         }
     }
 }
