fd51c13931884e31511b56ac4e915442b120d780
Stephen Gesin <sgesin@salesforce.com>
2020-11-02 15:41:39 -0800

    config updates

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 const jestPreset = require('@lwc/jest-preset');
+const setupFilesAfterEnv = jestPreset.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 module.exports = {
     ...jestConfig,
     resolver: '<rootDir>/bin/bcr-resolver.js',
