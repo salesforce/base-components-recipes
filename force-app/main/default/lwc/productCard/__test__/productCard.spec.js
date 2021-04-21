07720058721b394fa070571c67d671728b8be286
Ryan Panos <rpanos@salesforce.com>
2021-04-20 21:29:35 -0700

    Failed test
    npm run test:unit productCard
    
    Gives:
      ● Test suite failed to run
    
        TypeError: Invalid adapterId, it must be extensible.
    
          at node_modules/@lwc/jest-preset/src/setup.js:139:23
              at Array.forEach (<anonymous>)
          at overriddenRegisterDecorators (node_modules/@lwc/jest-preset/src/setup.js:126:23)
          at Object.<anonymous> (force-app/main/default/lwc/productCard/productCard.js:36:29)
          at Object.<anonymous> (force-app/main/default/lwc/productCard/__test__/productCard.spec.js:5:1)

---
+++ force-app/main/default/lwc/productCard/__test__/productCard.spec.js
@@ -0,0 +1,37 @@
+import {
+    createElement
+} from 'lwc';
+import {registerLdsTestWireAdapter} from '@salesforce/sfdx-lwc-jest';
+import ProductCard from 'c/productCard'; // updated - grabbing locally instead of   ../productCard
+import {getRecord} from 'lightning/uiRecordApi';
+
+// Import mock data to send through the wire adapter.
+const mockGetRecord = require('./data/getRecord.json');
+
+// Register a test wire adapter. - 2.0 way!
+const getRecordWireAdapter = registerLdsTestWireAdapter(getRecord);
+
+describe('@wire demonstration test', () => {
+// Disconnect the component to reset the adapter. It is also
+// a best practice to clean up after each test.
+    afterEach(() => {
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('displays product name field', () => {
+        const element = createElement('c-product_filter', {
+            is: ProductCard
+        });
+        document.body.appendChild(element);
+        getRecordWireAdapter.emit(mockGetRecord);
+
+// Resolve a promise to wait for a rerender of the new content.
+        return Promise.resolve().then(() => {
+            const content = element.querySelector('.content');
+            const nameField = mockGetRecord.fields.Name.value;
+            expect(content.textContent).toBe(`Name:${nameField}`)
+        });
+    });
+});
