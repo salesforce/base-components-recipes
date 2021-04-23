8a411116ee78c405b64acf24e0e489e3bb607b8f
Ryan Panos <rpanos@salesforce.com>
2021-04-23 09:27:18 -0700

    Cleaned and works!!

--- force-app/main/default/lwc/productCard/__test__/productCard.spec.js
+++ force-app/main/default/lwc/productCard/__test__/productCard.spec.js
@@ -2,18 +2,13 @@ import {
     createElement
 } from 'lwc';
 import {registerLdsTestWireAdapter} from '@salesforce/sfdx-lwc-jest';
-// import ProductCard from '../productCard'; // updated - grabbing locally instead of c/productCard - not extensible
 import ProductCard from 'c/productCard';
 import {getRecord} from 'lightning/uiRecordApi'; // NOW importing MODERN mocks.  Maybe this needs to be real?
-
-
 
 // Import mock data to send through the wire adapter.
 const mockGetRecord = require('./getRecord.json');
 
 // Register a test wire adapter. - 2.0 way!
-// const getRecordWireAdapter = registerLdsTestWireAdapter("getRecord");
-console.warn(`JOSE 2`, getRecord);
 const getRecordAdapter = registerLdsTestWireAdapter(getRecord);
 
 describe('@wire demonstration test', () => {
@@ -26,29 +21,18 @@ describe('@wire demonstration test', () => {
     });
 
     it('displays product name field', () => {
-        debugger;
         const element = createElement('c-product_filter', {
             is: ProductCard
         });
         document.body.appendChild(element);
-        debugger;
         getRecordAdapter.emit(mockGetRecord);
 
 // Resolve a promise to wait for a rerender of the new content.
         return Promise.resolve().then(() => {
-            debugger;
-
             const content = element.shadowRoot.querySelector('.content');
             const name = content.querySelector('.name');
-            // console.warn(`content:`)
-            // console.error(content)
-            // console.error(content.shadowRoot);
-            console.error(mockGetRecord);
-            console.error(mockGetRecord.fields);
-            console.error(mockGetRecord.fields.Name);
 
             const fields = mockGetRecord.fields;
-            console.error(fields);
 
             const nameField = fields.Name.value;
             expect(name.textContent).toBe(`Name:${nameField}`); // both not null
--- force-app/main/default/lwc/productCard/productCard.js
+++ force-app/main/default/lwc/productCard/productCard.js
@@ -1,53 +1,23 @@
-import {api, LightningElement, wire} from 'lwc';
-
-// Wire adapter to load records.
-console.warn("LOCAL productCard");
-debugger;
+import { LightningElement, wire } from 'lwc';
 
 import { getRecord } from 'lightning/uiRecordApi';
-console.warn(`JOSE `, getRecord);
 export default class ProductCard extends LightningElement {
     // Id of Product__c to display.
-    // @api
     recordId;
 
     // Product__c to display //
-    // @api
     product;
 
     // Product__c field values to display. //
-    // @api
     name = '';
 
-    /*
-    Trevor example
-    @wire(getRecord, { recordId: '$recordId', ['Contact.Name']})
-     */
-
     @wire(getRecord, { recordId: '$recordId',
-    // fields
         fields: ["Product__c.Name"]
     })
-    wiredRecord({ data, error}) {
-        debugger
-        console.error(error)
-        console.error('DOING WIRE')
-        console.error(data)
+    wiredRecord({ data }) {
         if (data) {
-            console.error('Have Data WIRE')
             this.product = data;
             this.name = data.fields.Name.value;
-        } else {
-            console.error('DID NOT Have Data WIRE')
         }
     }
-
-    /*
-    @wire(getRecordCreateDefaults, {
-        objectApiName: '$getRecordCreateDefaultsConfig.data.objectApiName',
-        recordTypeId: '$recordTypeId',
-        optionalFields: '$getRecordCreateDefaultsConfig.data.optionalFields',
-    })
-    wiredRecordUiCreate;
-     */
 }
